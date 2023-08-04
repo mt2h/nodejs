24475f061639264c19ab64fe946f30d86851dd24
Cristhian Monasterio <cristhian.monasterio@gmail.com>
2023-08-04 04:40:05 +0000

    first example

--- section3/app.js
+++ section3/app.js
@@ -1,3 +1,13 @@
 const fs = require('fs')
 
 fs.writeFileSync('notes.txt', 'This file was created with Node.js!')
+
+//const name = require('./utils.js')
+//console.log(name)
+
+const add = require('./utils.js')
+console.log(add(5, 5))
+
+const getNotes = require('./notes.js')
+const msg = getNotes()
+console.log(msg)
