270f298df3529c2885ef0d41ea418a4aa78a54da
Cristhian Monasterio <cristhian.monasterio@gmail.com>
2023-08-31 15:33:11 +0000

    example callback

--- section6/callback.js
+++ section6/callback.js
@@ -1,11 +1,36 @@
-console.log('Starting')
-
+//asynchronous
 setTimeout(() => {
-    console.log('2 Second Timer ')
+    console.log('Two seconds are up')
     }, 2000)
 
-    setTimeout(() => {
-        console.log('0 Second Timer ')
-        }, 0)
+//synchronous
+const names = ['Andrew', 'Jen', 'Jess']
+const shortNames = names.filter((name) => {
+    return name.length <= 4
+    })
 
-console.log('Stopping')
+console.log(shortNames)
+
+const geocode = (address, callback) => {
+    setTimeout(() => {
+        const data = {
+            latitude: 0,
+            longitude: 0
+            }
+        callback(data) //return when is a callback
+        }, 2000)
+    }
+
+geocode('Philadelphia', (data) => {
+    console.log(data)
+    })
+
+const add = (a, b, callback) => {
+    setTimeout(() => {
+        callback(a + b)
+        }, 2000)
+    }
+
+add(1, 4, (sum) => {
+    console.log(sum)
+    })
