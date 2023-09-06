8e6f1732560b293a77f2ac00a860fd5d35609d08
Cristhian Monasterio <cristhian.monasterio@gmail.com>
2023-09-06 00:20:55 +0000

    example destructuring

--- section6/app.js
+++ section6/app.js
@@ -4,7 +4,7 @@ const forecast = require('./utils/forecast')
 //console.log(process.argv)
 const address = process.argv[2]
 
-if(!address){
+/*if(!address){
     console.log('Please provide an anddress')
     }
 else{
@@ -20,6 +20,24 @@ else{
                 }
             console.log(data.location)
             //console.log('Error', error)
+            console.log('Data', dataForecast)
+            })
+        })
+    }*/
+
+if(!address){
+    console.log('Please provide an anddress')
+    }
+else{
+    geocode(address, (error, {latitude, longitude, location} = {}) => { //= {} default null object value
+        if(error){
+            return console.log(error)
+            }
+        forecast(latitude, longitude, (error, dataForecast) => {
+            if(error){
+                return console.log(error)
+                }
+            console.log(location)
             console.log('Data', dataForecast)
             })
         })
--- section6/utils/geocode.js
+++ section6/utils/geocode.js
@@ -1,6 +1,6 @@
 const request = require('request')
 
-const geocode = (address, callback) => {
+/*const geocode = (address, callback) => {
     const url = 'http://api.weatherstack.com/current?access_key=5d6f3e9cadeab1e30ee65c20129c32f8&query='+ address //+'&units=f'
 
     request({url: url, json: true}, (error, response) => {
@@ -18,6 +18,27 @@ const geocode = (address, callback) => {
                 })
             }
         })
-    }
+    }*/
+
+const geocode = (address, callback) => {
+        const url = 'http://api.weatherstack.com/current?access_key=5d6f3e9cadeab1e30ee65c20129c32f8&query='+ address //+'&units=f'
+    
+        request({url, json: true}, (error, {body} = {}) => {
+            if(error){
+               callback('Unable to connect to location services!', undefined)
+                }
+            else if(body.current === undefined){
+                callback('Unable to find location. Try another search!', undefined)
+                }
+            else{
+                callback(undefined, {
+                    latitude: body.location.lat,
+                    longitude: body.location.lon,
+                    location: body.location.name
+                    })
+                }
+            })
+        }
+    
 
 module.exports = geocode
